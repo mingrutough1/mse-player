e23cc17a2af71be0baac97b9487bf7d0c33a7e50
funkyxia <funkyxia@tencent.com>
2025-01-13 11:12:28 +0800

    1.0.10

--- dist/bundle.esm.js
+++ dist/bundle.esm.js
@@ -1313,7 +1313,7 @@ var Touch = /** @class */ (function () {
         this.node.removeEventListener("mousedown", this.handleMousedown);
         document.removeEventListener("mousemove", this.handleMouseover);
         document.removeEventListener("mouseup", this.handleMouseup);
-        this._removePointerLockEvent();
+        this.isPc && this._removePointerLockEvent();
         this.hasBind = false;
     };
     return Touch;
--- dist/bundle.umd.js
+++ dist/bundle.umd.js
@@ -1317,7 +1317,7 @@
             this.node.removeEventListener("mousedown", this.handleMousedown);
             document.removeEventListener("mousemove", this.handleMouseover);
             document.removeEventListener("mouseup", this.handleMouseup);
-            this._removePointerLockEvent();
+            this.isPc && this._removePointerLockEvent();
             this.hasBind = false;
         };
         return Touch;
